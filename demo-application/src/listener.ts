fce2e680e2ff51c6ae261bace0f99da85208e269
Suri <maxsurii@outlook.com>
2021-09-28 12:13:18 +1000

    blockchain dev vscode setup

---
+++ demo-application/src/listener.ts
@@ -0,0 +1,53 @@
+import { Gateway, Wallets, ContractListener, Wallet, Network, Contract } from 'fabric-network';
+import * as path from 'path';
+import * as fs from 'fs';
+
+async function main(): Promise<void> {
+  try {
+
+    // Create a new file system based wallet for managing identities.
+    const walletPath: string = path.join(process.cwd(), 'Org1Wallet');
+    const wallet: Wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway: Gateway = new Gateway();
+    const connectionProfilePath: string = path.resolve(__dirname, '..', 'connection.json');
+    const connectionProfile: any = JSON.parse(fs.readFileSync(connectionProfilePath, 'utf8')); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+    const connectionOptions: any = { wallet, identity: 'Org1 Admin', discovery: { enabled: true, asLocalhost: true }};
+    await gateway.connect(connectionProfile, connectionOptions);
+
+    // Get the network (channel) our contract is deployed to.
+    const network: Network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract: Contract = network.getContract('demo-contract'); 
+
+
+    // Listen for myEvent publications
+    const listener: ContractListener = async (event) => {    // eslint-disable-line @typescript-eslint/require-await
+      if (event.eventName === 'myEvent') {
+        console.log( 'chaincodeId: ', event.chaincodeId , ' eventName: ' , event.eventName , ' payload: ' , event.payload?.toString());
+      }
+    };
+
+    const finished: boolean = false;
+    await contract.addContractListener(listener);
+
+    console.log('Listening for myEvent events...');
+    while (!finished) {
+      await sleep(5000);
+      // ... do other things
+    }
+
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+}
+
+function sleep(ms: number): Promise<any> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+void main();
---
+++ demo-contract/lib/my-asset-contract.js
@@ -0,0 +1,87 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class MyAssetContract extends Contract {
+
+    async myAssetExists(ctx, myAssetId) {
+        const buffer = await ctx.stub.getState(myAssetId);
+        return (!!buffer && buffer.length > 0);
+    }
+
+    async createMyAsset(ctx, myAssetId, value) {
+        const exists = await this.myAssetExists(ctx, myAssetId);
+        if (exists) {
+            throw new Error(`The my asset ${myAssetId} already exists`);
+        }
+        const asset = { value };
+        const buffer = Buffer.from(JSON.stringify(asset));
+        await ctx.stub.putState(myAssetId, buffer);
+        const eventPayload = Buffer.from(`Created asset ${myAssetId} (${value})`);
+        ctx.stub.setEvent('myEvent', eventPayload);
+    }
+
+    async readMyAsset(ctx, myAssetId) {
+        const exists = await this.myAssetExists(ctx, myAssetId);
+        if (!exists) {
+            throw new Error(`The my asset ${myAssetId} does not exist`);
+        }
+        const buffer = await ctx.stub.getState(myAssetId);
+        const asset = JSON.parse(buffer.toString());
+        return asset;
+    }
+
+    async updateMyAsset(ctx, myAssetId, newValue) {
+        const exists = await this.myAssetExists(ctx, myAssetId);
+        if (!exists) {
+            throw new Error(`The my asset ${myAssetId} does not exist`);
+        }
+        const asset = { value: newValue };
+        const buffer = Buffer.from(JSON.stringify(asset));
+        await ctx.stub.putState(myAssetId, buffer);
+    }
+
+    async deleteMyAsset(ctx, myAssetId) {
+        const exists = await this.myAssetExists(ctx, myAssetId);
+        if (!exists) {
+            throw new Error(`The my asset ${myAssetId} does not exist`);
+        }
+        await ctx.stub.deleteState(myAssetId);
+    }
+
+    async queryAllAssets(ctx) {
+        const startKey = '000';
+        const endKey = '999';
+        const iterator = await ctx.stub.getStateByRange(startKey, endKey);
+        const allResults = [];
+        while (true) {
+            const res = await iterator.next();
+            if (res.value && res.value.value.toString()) {
+                console.log(res.value.value.toString());
+
+                const Key = res.value.key;
+                let Record;
+                try {
+                    Record = JSON.parse(res.value.value.toString());
+                } catch (err) {
+                    console.log(err);
+                    Record = res.value.value.toString();
+                }
+                allResults.push({ Key, Record });
+            }
+            if (res.done) {
+                console.log('end of data');
+                await iterator.close();
+                console.info(allResults);
+                return JSON.stringify(allResults);
+            }
+        }
+    }
+
+}
+
+module.exports = MyAssetContract;
